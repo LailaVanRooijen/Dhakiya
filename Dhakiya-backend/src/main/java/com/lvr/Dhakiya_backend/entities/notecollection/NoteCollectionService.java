1556ca4acf0b8021334e1eaa8634f3c7e240b621
Laila <175353500+LailaVanRooijen@users.noreply.github.com>
2024-12-22 16:44:04 +0100

    note to self check for bugs before committing...

--- Dhakiya-backend/src/main/java/com/lvr/Dhakiya_backend/entities/notecollection/NoteCollectionService.java
+++ Dhakiya-backend/src/main/java/com/lvr/Dhakiya_backend/entities/notecollection/NoteCollectionService.java
@@ -10,6 +10,10 @@ import org.springframework.stereotype.Service;
 public class NoteCollectionService {
   private final NoteCollectionRepository noteCollectionRepository;
 
+  public List<NoteCollection> getAll() {
+    return noteCollectionRepository.findAll();
+  }
+
   public NoteCollection getById(Long id) {
     return noteCollectionRepository.findById(id).orElseThrow(NotFoundException::new);
   }
